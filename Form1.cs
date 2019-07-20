45df5194b5d4eacb72ffee511655af84248e8c11
Jerome <jerome6668@qq.com>
2019-07-20 16:44:37 +0800

    测试提交

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace TestCef
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("http://192.168.0.54:808/start.html");
+            panel1.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+    }
+}
